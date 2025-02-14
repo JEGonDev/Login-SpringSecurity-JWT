a9a2be61be50c0aa77e72d4e89f8b5c68ae56ae9
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2025-02-14 12:11:54 -0500

    se actualiza el metodo register para crear usuario, codificar contrasenia, asignarle el rol user y guardarlo en la DB, ahora devuelve un token JWT para la autenticacion de usuario

--- src/main/java/JEGonDev/Login/JWT/SpringSecurity/Auth/AuthService.java
+++ src/main/java/JEGonDev/Login/JWT/SpringSecurity/Auth/AuthService.java
@@ -1,17 +1,37 @@
 package JEGonDev.Login.JWT.SpringSecurity.Auth;
 
+import JEGonDev.Login.JWT.SpringSecurity.User.Role;
+import JEGonDev.Login.JWT.SpringSecurity.User.User;
+import JEGonDev.Login.JWT.SpringSecurity.User.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+
     public AuthResponse login(LoginRequest request) {
         return null;
     }
 
-    public AuthResponse register(LoginRequest request) {
-        return null;
+    public AuthResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .username(request.getUsername())
+                .password(passwordEncoder.encode( request.getPassword()))
+                .firstname(request.getFirstname())
+                .lastname(request.lastname)
+                .country(request.getCountry())
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+
+        return AuthResponse.builder()
+                .token(jwtService.getToken(user))
+                .build();
     }
 }
---
+++ src/main/java/JEGonDev/Login/JWT/SpringSecurity/Auth/JwtService.java
@@ -0,0 +1,8 @@
+package JEGonDev.Login.JWT.SpringSecurity.Auth;
+
+import JEGonDev.Login.JWT.SpringSecurity.User.User;
+
+public class JwtService {
+    public String getToken(User user) {
+    }
+}
