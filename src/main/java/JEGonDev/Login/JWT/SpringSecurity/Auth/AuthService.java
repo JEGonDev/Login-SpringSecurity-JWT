48398df4b0f86abc8d99e90f7f201bd287905c26
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2025-02-14 17:07:45 -0500

    Se soluciona error , no se reconoce clase en AuthController

--- src/main/java/JEGonDev/Login/JWT/SpringSecurity/Auth/AuthService.java
+++ src/main/java/JEGonDev/Login/JWT/SpringSecurity/Auth/AuthService.java
@@ -5,6 +5,9 @@ import JEGonDev.Login.JWT.SpringSecurity.User.Role;
 import JEGonDev.Login.JWT.SpringSecurity.User.User;
 import JEGonDev.Login.JWT.SpringSecurity.User.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +18,15 @@ public class AuthService {
     private final UserRepository userRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     public AuthResponse login(LoginRequest request) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
     }
 
     public AuthResponse register(RegisterRequest request) {
